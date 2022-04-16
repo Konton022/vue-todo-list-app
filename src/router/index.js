132b3f825609902d96696b93252242d40d604c25
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-04-16 17:08:19 +0500

    gh-page router bag was fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LogIn from '../components/LogIn';
 import TodoApp from '../components/TodoApp';
 import PageNotFounded from '../components/PageNotFounded';
 
 const router = createRouter({
-    history: createWebHistory('/vue-todo-list-app/'),
+    history: createWebHashHistory('/vue-todo-list-app/'),
     routes: [
-        { path: '', component: LogIn },
-        { path: '/todoapp', component: TodoApp },
+        { path: '/', component: LogIn },
+        { path: '/todoapp/', component: TodoApp },
         { path: '/:pathMatch(.*)', component: PageNotFounded },
     ],
 });
