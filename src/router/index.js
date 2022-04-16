e4fd147fd16b7b6837b443d6dcd4c14162d04b08
Konton022 <42418105+Konton022@users.noreply.github.com>
2022-04-16 16:15:08 +0500

    add pagenotfouded

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LogIn from '../components/LogIn';
 import TodoApp from '../components/TodoApp';
+import PageNotFounded from '../components/PageNotFounded';
 
 const router = createRouter({
-    base: 'https://konton022.github.io/vue-todo-list-app/',
     history: createWebHistory('/vue-todo-list-app/'),
     routes: [
         { path: '', component: LogIn },
         { path: '/todoapp', component: TodoApp },
+        { path: '*', component: PageNotFounded },
     ],
 });
 
